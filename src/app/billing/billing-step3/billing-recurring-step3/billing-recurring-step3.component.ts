3eafdd1eb96348bb42db1c6067d9f051044da5df
spurthisn <spurthi.n@mitosistech.com>
2018-11-16 19:07:19 +0530

    Navigating to Billing Model Step 4 recurring

--- src/app/billing/billing-step3/billing-recurring-step3/billing-recurring-step3.component.ts
+++ src/app/billing/billing-step3/billing-recurring-step3/billing-recurring-step3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-billing-recurring-step3',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./billing-recurring-step3.component.scss']
 })
 export class BillingRecurringStep3Component implements OnInit {
-  // model: any = {}
-  constructor() {}
+  public model;
+  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.model = {
+      rupees: '',
+      amount: '',
+      No1: '',
+      No2: '',
+      No3: '',
+      Period1: '',
+      Period2: ''
+    };
+  }
+  onSubmit(data) {
+    console.log(this.model);
+    if (data.value) {
+      this.router.navigate(['billing/step4/recurring']);
+    }
+  }
 }

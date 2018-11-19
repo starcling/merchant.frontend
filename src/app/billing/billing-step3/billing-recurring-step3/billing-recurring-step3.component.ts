937271eb3608b3b7b4a89ed4924459565a1a2110
madhan <madhanaraj.i@mitosistech.com>
2018-11-19 19:28:09 +0530

    Committed Billing Step3

--- src/app/billing/billing-step3/billing-recurring-step3/billing-recurring-step3.component.ts
+++ src/app/billing/billing-step3/billing-recurring-step3/billing-recurring-step3.component.ts
@@ -8,7 +8,49 @@ import { Router } from '@angular/router';
 })
 export class BillingRecurringStep3Component implements OnInit {
   public model;
-  constructor(private router: Router) {}
+  public selectOption: any;
+  public recurrenceOption = [];
+  constructor(private router: Router) {
+    this.selectOption = [
+      {
+        id: 1,
+        label: 'USD',
+        value: 0
+      },
+      {
+        id: 2,
+        label: 'EUR',
+        value: 1
+      },
+      {
+        id: 3,
+        label: 'GBP',
+        value: 2
+      },
+      {
+        id: 4,
+        label: 'JPY',
+        value: 4
+      }
+    ];
+    this.recurrenceOption = [
+      {
+        id: 1,
+        label: 'Days',
+        value: 0
+      },
+      {
+        id: 2,
+        label: 'Weeks',
+        value: 1
+      },
+      {
+        id: 3,
+        label: 'Years',
+        value: 2
+      }
+    ];
+  }
 
   ngOnInit() {
     this.model = {
@@ -20,6 +62,9 @@ export class BillingRecurringStep3Component implements OnInit {
       Period1: '',
       Period2: ''
     };
+    this.model.Period2 = this.recurrenceOption[0].label;
+    this.model.Period1 = this.recurrenceOption[0].label;
+    this.model.rupees = this.selectOption[0].label;
   }
   onSubmit(data) {
     console.log(this.model);
